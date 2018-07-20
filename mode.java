6220b1417377e3dbe9cef0587c6cb04785852ce5
zlin13 <zlin13@stevens.edu>
2018-07-20 13:58:42 -0400

    Add files via upload

---
+++ mode.java
@@ -0,0 +1,16 @@
+public static int mode(int a[]) {
+    int maxValue, maxCount;
+
+    for (int i = 0; i < a.length; ++i) {
+        int count = 0;
+        for (int j = 0; j < a.length; ++j) {
+            if (a[j] == a[i]) ++count;
+        }
+        if (count > maxCount) {
+            maxCount = count;
+            maxValue = a[i];
+        }
+    }
+
+    return maxValue;
+}
