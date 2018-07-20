3119b7f7f8edc3bf60d610eb1dd2d4dedecc0864
zlin13 <zlin13@stevens.edu>
2018-07-20 13:57:17 -0400

    Add files via upload

---
+++ median.java
@@ -0,0 +1,8 @@
+public static double median(double[] m) {
+    int middle = m.length/2;
+    if (m.length%2 == 1) {
+        return m[middle];
+    } else {
+        return (m[middle-1] + m[middle]) / 2.0;
+    }
+}
